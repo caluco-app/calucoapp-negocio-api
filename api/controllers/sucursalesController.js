b5bf0aabf199239a0a4e83723bd003f2e142b050
luis-guzman-97 <luisespinoza.le108@gamil.com>
2024-03-24 10:49:52 -0600

    Servicios de sucursal

--- api/controllers/sucursalesController.js
+++ api/controllers/sucursalesController.js
@@ -50,3 +50,35 @@ exports.obtenerPermisos = (req, res) => {
         }
     });
 };
+
+exports.obtnerfacturasPorSucursal = (req, res) => {
+    const idsucursal = req.params.idsucursal; // Suponiendo que el ID del negocio se pasa como parámetro en la URL
+    const idsubopcion = req.params.idsubopcion; // Suponiendo que el ID del negocio se pasa como parámetro en la URL
+
+    // Consulta para obtener productos por negocio
+    const query = `
+    select 
+    s2.nombre as tipo, 
+    f.id as idfactura, f.idsucursal, f.idsubopcion, f.idusuario, f.fechacreacion, f.fecha , f.codigofactura , f.idsocio, 
+    f.idperiodotributario , f.estadotributario , s.nombre as socio, e.nombrecompleto  as empleado, u.usuario 
+    from facturas f  
+    join socios s on s.id =f.idsocio
+    join subopcionessucursal s2 on s2.id = f.idsubopcion 
+    join usuariopornegocio u on u.id = f.idusuario 
+    join empleadosnegocio e on e.id=u.idempleado 
+    and f.idsubopcion = ? and f.idsucursal =?
+    `;
+
+    db.query(query, [idsubopcion, idsucursal], (err, resultados) => {
+        if (err) {
+            console.error('Error en la consulta:', err);
+            return res.status(200).json({ state: 'fail', data: [], message: 'Error en la consulta a la base de datos' });
+        }
+        // Verificar si se encontraron productos
+        if (resultados.length > 0) {
+            return res.json({ state: 'success', data: resultados, message: 'Registros obtenido correctamente' });
+        } else {
+            return res.status(200).json({ state: 'fail', data: [], message: 'No se encontraron productos para el negocio dado' });
+        }
+    });
+};
--- api/routes/sucursalesRoutes.js
+++ api/routes/sucursalesRoutes.js
@@ -4,5 +4,6 @@ const controllerSucursal = require('../controllers/sucursalesController');
 const sucursalRouter = express.Router();
 
 sucursalRouter.get('/permisosPorSucursal/:id', controllerSucursal.obtenerPermisos);
+sucursalRouter.get('/obtnerComprobantesPorSucursal/:idsubopcion/:idsucursal', controllerSucursal.obtnerfacturasPorSucursal);
 
 module.exports = sucursalRouter;
